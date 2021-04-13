9ea65b66182aa656c7f51ca6c4644c5db75727ae
GabrielLdeMoraes <80229837+GabrielLdeMoraes@users.noreply.github.com>
2021-04-13 00:16:08 -0300

    Update1.5
    Alguns updates, correções e finalização do prototipo do primeiro mapa.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager instance;
+
+    public Transform respawnPoint;
+    public GameObject playerPrefab;
+
+    public CinemachineVirtualCameraBase Cam;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Respawn()
+    {
+        GameObject player = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+        Cam.Follow = player.transform;
+    }
+}
